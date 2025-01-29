50b187ee0c3392f99bc72d9207114b0a07b0e536
Michael <michael.dokychuk@qaracter.com>
2025-01-29 10:03:48 +0000

    Added classes and basic project structure

---
+++ src/Cart.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    private List<Item> items;
+
+    public Cart() {
+        items = new ArrayList<>();
+    }
+
+    public void addToCart(Item item, int quantity) throws IllegalArgumentException {
+
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity must be > 0.");
+        }
+        if (item.getQuantity() < quantity) {
+            throw new IllegalArgumentException("Not enough stock for " + item.getName());
+        }
+
+        Item cartItem = new Item(item.getName(), item.getPrice(), quantity);
+        items.add(cartItem);
+        item.setQuantity(item.getQuantity() - quantity);
+        System.out.println(quantity + " " + item.getName() + "s added to the cart.");
+
+    }
+
+    public double totalPrice() {
+        double total = 0;
+
+        for (Item item : items) {
+            total += item.getPrice() * item.getQuantity();
+        }
+        //Using Math class method.
+        return Math.round(total * 100.0) / 100.0;
+    }
+
+    public double applyDiscount(double total, double discountPercentage) {
+        return total * (1 - discountPercentage / 100);
+    }
+
+    public void displayCart() {
+        System.out.println("Cart contents: ");
+
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+}
---
+++ src/Inventory.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory {
+    private List<Item> items;
+
+    public Inventory() {
+        items = new ArrayList<>();
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void displayInventory() {
+        System.out.println("Inventory: ");
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
+
+    public Item findItem(String name) {
+        for (Item item : items) {
+            if (item.getName().equalsIgnoreCase(name)) {
+                return item;
+            }
+        }
+        return null;
+    }
+}
